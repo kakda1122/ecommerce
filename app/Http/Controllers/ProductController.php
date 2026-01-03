eeabf774c488378c6d1be5f4efa8a9ff25b5bbd8
chheang Kakda <kakda1897@gmail.com>
2026-01-03 07:39:48 +0700

    TP5 update some products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,43 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+   // --- Get /api/products
+    public function getCategories()
+    {
+        return [
+            "message" => "Getting list of products"
+        ];
+    }
+
+    // --- Post /api/products
+    public function createProduct()
+    {
+        return [
+            "message" => "Creating 1 new product"
+        ];
+    }
+
+    // --- Get /api/products/{productId}
+    public function getProduct($productId)
+    {
+        return [
+            "message" => "Getting 1 product base on given productId"
+        ];
+    }
+
+    // --- Patch /api/products/{productId}
+    public function updateProduct($productId)
+    {
+        return [
+            "message" => "Updating 1 product base on given productId"
+        ];
+    }
+
+    // --- Delete /api/products/{productId}
+    public function deleteProduct($productId)
+    {
+        return [
+            "message" => "Deleting 1 product base on given productId"
+        ];
+    }
 }
